155dc73274df96f6504940aadf840defad65a778
Jak Spalding <jak@moo.com>
2014-12-08 09:55:34 +0000

    hub now installed via brew

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,4 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
+alias git=hub
 
 # The rest of my fun git aliases
 alias ga='git add'
